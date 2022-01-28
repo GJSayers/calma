81e7da0b7c701c591b698b46f7331d875cab8b5b
David <davidpkelly@me.com>
2022-01-28 15:34:28 +0000

    Setup JS modules

---
+++ html/main.js
@@ -0,0 +1,4 @@
+import Timer from "./timer.js";
+
+new Timer(
+document.querySelector(".timer"));
--- html/timer.js
+++ html/timer.js
@@ -1,15 +1,30 @@
-const displayTime = document.getElementById("displayTime")
-const btnStart = document.getElementById("btnStart");
-const btnReset = document.getElementById("btnReset");
+export default class Timer {
+    constructor(root) {
+        root.innerHTML = Timer.getHTML();
 
-const timerStart = Date.now();
-let timerEnd;
+        this.el = {
+            mins: root.querySelector(".timer-part-mins"),
+            secs: root.querySelector(".timer-part-secs"),
+            control: root.querySelector(".timer-btn-control"),
+            reset: root.querySelector(".timer-btn-reset")
+        };
 
-btnStart.addEventListener("click", runTimer)
+        console.log(this.el)
+        
+    }
 
-console.log(timerStart)
-
-
-function runTimer() {
-    console.log("Timer running!");
+    static getHTML() {
+        return `
+        <span class="timer-part timer-part-mins">00</span>
+        <span class="timer-part">:</span>
+        <span class="timer-part timer-part-secs">00</span>
+        <br>
+        <button type="button" class="timer-btn timer-btn-control timer-btn-start">
+            <i class="bi bi-play-fill"></i>
+        </button>
+        <button type="button" class="timer-btn timer-btn-reset">
+            <i class="bi bi-stopwatch"></i>
+        </button> 
+        `;
+    }
 }
