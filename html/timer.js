353b8cc933efdba06b90cc6f6230340d79c6d8cb
David <davidpkelly@me.com>
2022-01-28 13:46:48 +0000

    Add basic timer files

---
+++ html/timer.js
@@ -0,0 +1,8 @@
+const displayTime = document.getElementById("displayTime")
+// const btnTime15 = document.getElementById("btnTime15");
+// const btnTime25 = document.getElementById("btnTime25");
+// const btnTime35 = document.getElementById("btnTime35");
+const btnPause = document.getElementById("btnPause");
+const btnReset = document.getElementById("btnReset");
+
+let startingMins = 25;
