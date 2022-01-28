2048519fa7f10fbe6dca03daf403d8361d345f90
GJSayers <gemma.sayers1@btinternet.com>
2022-01-28 14:39:40 +0000

    Wire up home to display dashboard urls

--- calma/urls.py
+++ calma/urls.py
@@ -18,6 +18,7 @@ from django.urls import path
 from django.conf.urls import include
 
 urlpatterns = [
+    path('', include('dashboard.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('dashboard/', include('dashboard.urls')),
