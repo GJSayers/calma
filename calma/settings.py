5eeb5b4bd93bb56ee0aeb61188884bf020c83570
Rian Sunthbocus <rian@rians-mbp.speedport.ip>
2022-01-28 21:07:19 +0100

    Added the models to the aadmin dashboard

--- calma/settings.py
+++ calma/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = [os.environ.get('HOST')]
 
 
 # Application definition
