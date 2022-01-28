9bdd555d1ef84b1a9528b65fd41adb8cf424e83e
GJSayers <gemma.sayers1@btinternet.com>
2022-01-28 20:27:47 +0000

    Amend settings to enable css application

--- calma/urls.py
+++ calma/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('dashboard.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('dashboard/', include('dashboard.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
