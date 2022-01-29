02fc9618fb9fa26b1376ee429a347f4ef4b411cd
GJSayers <gemma.sayers1@btinternet.com>
2022-01-29 14:01:59 +0000

    Add local host

--- calma/settings.py
+++ calma/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.environ.get('HOST')]
+ALLOWED_HOSTS = [os.environ.get('HOST'), 'localhost©']
 
 
 # Application definition
