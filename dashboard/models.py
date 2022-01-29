9aa3ff1ca6a98a008e501608990a77af3cf87588
GJSayers <gemma.sayers1@btinternet.com>
2022-01-29 16:15:11 +0000

    Create activity model & wire up dboard app routes

--- dashboard/models.py
+++ dashboard/models.py
@@ -43,3 +43,21 @@ class MindfulCheck(models.Model):
 class Message(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     message_text = models.CharField(null=False, max_length=200)
+
+
+class Activities(models.Model):
+    title = models.CharField(max_length=50, null=False)
+    content = models.URLField(max_length=1024, null=False, blank=False)
+    vid_length = models.CharField(max_length=12, null=False)
+    WELLNESS_PREFS = (
+        ('yoga', 'Yoga'),
+        ('meditation', 'Meditation'),
+        ('music', 'Music'),
+        ('puzzles', 'Puzzles'),
+        ('gaming', 'Gaming'),
+        ('hiit', 'HIIT'),
+        ('reading', 'Reading'),
+    )
+
+    preferences = MultiSelectField(max_length=100, choices=WELLNESS_PREFS)
+
--- dashboard/views.py
+++ dashboard/views.py
@@ -4,16 +4,44 @@ from django.contrib.auth.models import User
 from .models import UserPreferences, MindfulCheck, Message
 
 
+def dashboard(request):
+    """
+    Display the dashboard apps with the logged 
+    """
+    mindful_checks = MindfulCheck.objects.all()
+
+    form = WellnessCheckForm()
+
+    if request.method == "POST":
+        print(request.POST)
+        form = WellnessCheckForm(request.POST)
+        if form.is_valid():
+            form.save()
+            
+    context = {
+        'form': form,
+        'mindful_checks': mindful_checks,
+    }
+    
+    return render(request, 'dashboard/index.html', context)
+
+
 def user_prefs(request):
     """
-    Display the u
+    Display the form for user preference selection / update
     """
     return render(request, 'dashboard/index.html')
 
 
 def message_self(request):
     """
-    Display the dashboard apps with the logged 
+    Display the form & handle submission for messages to future self
+    """
+    return render(request, 'dashboard/index.html')
+
+def activities(request):
+    """
+    Display the video options based on the user preferences, handle the onclick
     """
     return render(request, 'dashboard/index.html')
 
