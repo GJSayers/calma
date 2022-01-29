cd3b58b237754bca2a0eb11221db9e5a4422e3c0
GJSayers <gemma.sayers1@btinternet.com>
2022-01-29 11:27:38 +0000

    Add extra urls

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.dashboard, name='dashboard')
+    path('', views.user_prefs, name='user_prefs'),
+    path('', views.message_self, name='message_self')
 ]
