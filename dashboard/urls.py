583520f4e1d963fe513bff434362673491522058
Rian Sunthbocus <rian@rians-mbp.speedport.ip>
2022-01-30 15:16:42 +0100

    Added required pages and forms, trying to solve form bugs

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -3,8 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
-    path('', views.user_prefs, name='user_prefs'),
-    path('', views.message_self, name='message_self'),
+    # path('', views.user_prefs, name='user_prefs'),
+    path('message-self/', views.message_self_content, name='message_self'),
+    path('add-message-self/', views.add_message_self, name='add_message_self'),
     path('', views.activities, name='activities')
 ]
 
--- dashboard/views.py
+++ dashboard/views.py
@@ -7,45 +7,73 @@ from .models import UserPreferences, Message
 from .forms import UserPreferencesForm, MessageToSelfForm
 
 def dashboard(request):
-    if request.user.is_authenticated:
-        user = Message.objects.get(user=request.user)
-        print('user', user)
-        message_form = MessageToSelfForm()
-        message_text = Message.message_text
-        print('message_text', message_text)
-        form_data = {
-            'user': user,
-            'message_text': request.POST['message_text']
-        }
-        print('user', user),
-        if request.method == "POST":
-            print('request post', request.POST)
-            message_form = MessageToSelfForm(form_data)
-            print('message_form', user),
-            if message_form.is_valid():
-                message_form.save()
-    context = {'form': UserPreferencesForm(), 'message_text_form': MessageToSelfForm()}
+    
 
-    return render(request, 'dashboard/index.html', context)
-
-
-def user_prefs(request):
-    """
-    Display the form for user preference selection / update
-    """
     context = {'form': UserPreferencesForm()}
 
     return render(request, 'dashboard/index.html', context)
 
 
-def message_self(request):
+# def user_prefs(request):
+#     """
+#     Display the form for user preference selection / update
+#     """
+#     context = {'form': UserPreferencesForm()}
+
+#     return render(request, 'dashboard/index.html', context)
+    
+
+def message_self_content(request):
+    # Show individual news messages
+    
+    msg = Message.objects.all()
+
+    context = {
+        'message': msg
+    }
+
+    return render(request, 'dashboard/message_self_content.html', context)
+
+def add_message_self(request):
     """
     Display the form & handle submission for messages to future self
     """
+# if request.user.is_authenticated:
+    #     user = Message.objects.get(user=request.user)
+    #     print('user', user)
+    # message_form = MessageToSelfForm()
+    # message_text = Message.message_text
+    # print('message_text', message_text)
+    # form_data = {
+    #     # 'user': user,
+    #     'message_text': request.POST.get('message_text')
+    # }
+    # # print('user', user),
+    # if request.method == "POST":
+    #     print('request post', request.POST)
+    #     message_form = MessageToSelfForm(form_data)
+        # print('message_form', user),
+        # if message_form.is_valid():
+        #     msg = message_form.save(commit=False)
+        #     msg.user = request.user
+        #     msg.save()
+    profile = get_object_or_404(Message, user=request.user)
 
-    context = {'message_text_form': MessageToSelfForm()}
+    if request.method == "POST":
+        form = MessageToSelfForm(request.POST, instance=profile)
+        # form = Message(request.POST, request.FILES)
+        if form.is_valid():
+            msg = form.save()
+            # return redirect(reverse('message_content', args=[msg.id]))
+    else: 
+        form = MessageToSelfForm(instance=profile)
+    form = MessageToSelfForm()
 
-    return render(request, 'dashboard/index.html', context)
+    context = {'message_text_form': form}
+
+    return render(request, 'dashboard/add_message_self_modal.html', context)
+
+
     # if request.user.is_authenticated:
     #     user = UserPreferences.objects.get(user=request.user)
     #     print('user', user)
